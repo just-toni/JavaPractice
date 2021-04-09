2a5751c489bca132a4777ba2f2d180054af5da82
Oluwatoni Adeniyi <toniadeniyi14@gmail.com>
2021-04-09 23:22:10 +0100

    Practice

--- src/chapterSeven/EnhancedForLoop.java
+++ src/chapterSeven/EnhancedForLoop.java
@@ -6,15 +6,15 @@ public class EnhancedForLoop {
         String[] names = {"Rose", "Michael", "Jamal", "Rebecca", "Joy", "Sam Smith", "Jennifer", "Joshua", "Sarah"};
         int[] numbers = {1, 4, 2, 6, 7, 2, 4, 6, 8, 9};
 
-//        for (String name : names) {
-//            System.out.println(name);
-//        }
-//
-//        int total = 0;
-//        for (int number : numbers) {
-//            total += number;
-//        }
-//        System.out.println(total);
+        for (String name : names) {
+            System.out.println(name);
+        }
+
+        int total = 0;
+        for (int number : numbers) {
+            total += number;
+        }
+        System.out.println(total);
 
         int result = sum(numbers);
         System.out.println(result);
