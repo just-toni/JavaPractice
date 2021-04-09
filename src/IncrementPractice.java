e9b4092cc6bb6e098ca82537f00dd62987561271
Oluwatoni Adeniyi <toniadeniyi14@gmail.com>
2021-04-09 23:22:48 +0100

    Practice

--- src/IncrementPractice.java
+++ src/IncrementPractice.java
@@ -2,20 +2,20 @@ public class IncrementPractice {
 
     public static void main(String[] args) {
         int increment = 0;
-//        System.out.println("Result before post increment is:");
-//        System.out.println(increment);
-//        System.out.println("Result of post incrementing is:");
-//        System.out.println(increment++);
-//        System.out.println("Result of post increment is ");
-//        System.out.println(increment);
-//
-//        int increment2 = 0;
-//        System.out.println("Result before pre increment is:");
-//        System.out.println(increment2);
-//        System.out.println("Result of pre incrementing is:");
-//        System.out.println(++increment2);
-//        System.out.println("Result of pre increment is: ");
-//        System.out.println(increment2);
+        System.out.println("Result before post increment is:");
+        System.out.println(increment);
+        System.out.println("Result of post incrementing is:");
+        System.out.println(increment++);
+        System.out.println("Result of post increment is ");
+        System.out.println(increment);
+
+        int increment2 = 0;
+        System.out.println("Result before pre increment is:");
+        System.out.println(increment2);
+        System.out.println("Result of pre incrementing is:");
+        System.out.println(++increment2);
+        System.out.println("Result of pre increment is: ");
+        System.out.println(increment2);
 
         int c = 5 + ++increment;
         System.out.println(c);
